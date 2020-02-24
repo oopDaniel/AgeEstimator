ed3e4f7f232d9f233d5293110fa1c83f5276a907
Daniel <ssw1200@gmail.com>
2020-02-24 00:27:59 -0800

    add static method to parse label from file name

--- server/data/dataset.py
+++ server/data/dataset.py
@@ -14,18 +14,22 @@ from . import data_file_utils
 # In[20]:
 
 
-class DataLoader():
+class DataLoader(object):
     def __init__(self):
-        def to_label(x): return data_file_utils.get_age_by_file_name(
-            os.path.split(x)[-1])
 
         self.x_train = glob.glob(os.path.join(dirs.train_dir, "*.jpg"))
         self.x_test = glob.glob(os.path.join(dirs.test_dir, "*.jpg"))
-        self.y_train = list(map(to_label, self.x_train))
-        self.y_test = list(map(to_label, self.x_test))
+        self.y_train = list(map(DataLoader.to_label, self.x_train))
+        self.y_test = list(map(DataLoader.to_label, self.x_test))
 
         self.train_dir = dirs.train_dir
         self.test_dir = dirs.test_dir
+
+    @staticmethod
+    def to_label(full_path):
+        r"""Convert a full file path into its integer label"""
+        return data_file_utils.get_age_by_file_name(
+            os.path.split(full_path)[-1])
 
     def load_train(self):
         r"""File names of training data and their labels"""
