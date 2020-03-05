e923c21bb0d89340c2467ac279941b00be354fe2
Xiaoxiao Shang <xiaoxiao@Xiaoxiaos-MacBook-Pro-2.local>
2020-03-05 13:41:54 -0800

    add server/models/regression/predict.py

--- server/models/regression/predict.py
+++ server/models/regression/predict.py
@@ -25,14 +25,6 @@ class LogisticRegression(torch.nn.Module):
         return out
 
 
-test1 = Image.open('/Users/xiaoxiao/Desktop/SCU/2020winter/coen281/hw/termPro/AgeEstimator/server/data/dataset/test/26_utk_17029.jpg')
-# model = linearRegression(250*250, 121)
-# if os.path.exists(os.path.join(CURR_DIR, 'param')):
-#     model.load_state_dict(torch.load(os.path.join(CURR_DIR, 'param')))
-# else:
-#     print("No existent weight found. Train the network before using it.")
-#     raise FileNotFoundError
-
 def predict(img):
     r"""Predict the age of the given facial image.
     @Args:
@@ -61,5 +53,4 @@ def predict(img):
 
     return predicted.item()
 
-p = predict(test1)
-print(p)
+
