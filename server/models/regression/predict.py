08e3739d8bf1f3cdf64aae13b10273531f552dab
Daniel <ssw1200@gmail.com>
2020-03-05 22:35:17 -0800

    cleanup

--- server/models/regression/predict.py
+++ server/models/regression/predict.py
@@ -5,7 +5,7 @@ Import this module instead of calling it in CLI.
 
 import os
 import numpy as np
-from PIL import Image, ImageFilter 
+from PIL import Image, ImageFilter
 import torch
 from torch.autograd import Variable
 
@@ -34,23 +34,21 @@ def predict(img):
     """
 
     model = LogisticRegression(250*250, 121)
-    
+
     if os.path.exists(os.path.join(CURR_DIR, 'param')):
         model.load_state_dict(torch.load(os.path.join(CURR_DIR, 'param')))
     else:
         print("No existent weight found. Train the network before using it.")
         raise FileNotFoundError
-    
+
     img = np.array(img)
-    # print(type(img))
     img = Image.fromarray(img, 'RGB')
     image = img.convert('L')
-    image = image.filter(ImageFilter.UnsharpMask(radius=2, percent=150, threshold=3))
+    image = image.filter(ImageFilter.UnsharpMask(
+        radius=2, percent=150, threshold=3))
     image = np.array(image).reshape(1, 250*250)
     image_tensor = Variable(torch.Tensor(image))
     output = model(image_tensor)
     _, predicted = torch.max(output.data, 1)
 
     return predicted.item()
-
-
